0b8a696cd644c6f5841d039c769ab868fc696402
Dan <danbergelt@protonmail.com>
2019-10-08 18:47:11 -0400

    CRA generated, CRA boilerplate removed, CSS reset implemented

---
+++ spotter-client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './pages/Home';
+
+// Global Styles
+import './styles/global.scss';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
