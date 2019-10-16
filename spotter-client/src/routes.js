f8941479d45e1959fe982252c13b52d9a57c9aa6
Dan <danbergelt@protonmail.com>
2019-10-16 16:47:07 -0400

    implementing switch in routes component

--- spotter-client/src/routes.js
+++ spotter-client/src/routes.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { Route } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import LogIn from './pages/LogIn';
-import SignUp from './pages/SignUp';
-import NotFound from './pages/NotFound';
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import NotFound from "./pages/NotFound";
 
 const Routes = () => {
   return (
     <Layout>
-      <Route exact path="/" component={Home}/>
-      <Route path="/login" component={LogIn} />
-      <Route path="/signup" component={SignUp} />
-      <Route component={NotFound}/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={LogIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route component={NotFound} />
+      </Switch>
     </Layout>
-  )
-}
+  );
+};
 
 export default Routes;
