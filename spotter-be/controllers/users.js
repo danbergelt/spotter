570b84a2ea910a672144741d76412dc350ed8742
Dan <danbergelt@protonmail.com>
2019-11-01 14:00:28 -0400

    refactor user model

---
+++ spotter-be/controllers/users.js
@@ -0,0 +1,68 @@
+const Err = require("../utils/Err");
+const User = require("../models/User");
+const asyncHandler = require("../middleware/async");
+
+// @desc --> register user
+// @route --> POST /api/auth/register
+// @access --> Public
+
+exports.register = asyncHandler(async (req, res, next) => {
+  const { email, password, role } = req.body;
+
+  // create user
+  const user = await User.create({
+    email,
+    password,
+    role
+  });
+
+  sendToken(user, 200, res);
+});
+
+// @desc --> login user
+// @route --> POST /api/auth/login
+// @access --> Public
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(new Err("Please provide an email and password", 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new Err("Invalid credentials", 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new Err("Invalid credentials", 401));
+  }
+
+  sendToken(user, 200, res);
+});
+
+// Get token from model, send response
+const sendToken = (user, statusCode, res) => {
+  // Create token
+  const token = user.getToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res.status(statusCode).json({success: true, token})
+};
--- spotter-be/models/user.js
+++ spotter-be/models/user.js
@@ -1,17 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const Schema = mongoose.Schema;
 
 // User model
 const UserSchema = new Schema({
   email: {
     type: String,
+    trim: true,
     unique: true,
-    required: true,
+    required: [true, "Please add an email"],
+    match: [
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please add a valid email"
+    ]
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "Please add a password"],
+    minlength: [6, "Password needs to be at least 6 characters"],
+    select: false
+  },
+  role: {
+    type: String,
+    enum: ["user"],
+    default: "user"
+  },
+  created: {
+    type: Date,
+    default: Date.now
   }
 });
+
+// Encrypt password on save
+UserSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Sign token and return
+UserSchema.methods.getToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
+
+// Match password on login
+UserSchema.methods.matchPassword = async function(pw) {
+  return await bcrypt.compare(pw, this.password);
+};
 
 module.exports = User = mongoose.model("users", UserSchema);
