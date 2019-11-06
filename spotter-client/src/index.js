62b97f1489176d18a1a991f7a02a31bdcc75aa9c
Dan <danbergelt@protonmail.com>
2019-11-05 23:29:52 -0500

    redux boilerplate implemented

--- spotter-client/src/index.js
+++ spotter-client/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom";
 import Routes from "./routes";
 import "./styles/index.scss";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { reducer } from "./reducers/index";
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Router>
-    <Routes />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Routes />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
---
+++ spotter-client/src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+  test: "Hello World!"
+}
+
+export const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
