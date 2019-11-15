ce64ad35f7f73044f0c8535e2f9fd77f8af165f7
Dan <danbergelt@protonmail.com>
2019-11-15 17:26:45 -0500

    testing model

--- spotter-be/test/MODEL_UserCreate.test.js
+++ spotter-be/test/MODEL_UserCreate.test.js
@@ -18,14 +18,15 @@ describe("User model creation", () => {
   // Invalid email errs
   it("cannot create with invalid email", async () => {
     const user = new User({ email: "bademail", password: "password" });
-    await expect(user.save()).to.be.rejectedWith(Error);
+    await expect(user.save()).to.be.rejectedWith("Please add a valid email");
   });
 
   it("cannot create with no email", async () => {
     const user = new User({ password: "password" });
-    await expect(user.save()).to.be.rejectedWith(Error);
+    await expect(user.save()).to.be.rejectedWith("Please add an email");
   });
 
+  // Need to write custom error handler
   it("cannot create duplicate emails in collection", async () => {
     const user = new User({ email: "test@email.com", password: "password" });
     await user.save();
@@ -42,11 +43,11 @@ describe("User model creation", () => {
   // Invalid password errs
   it("cannot create user with no password", async () => {
     const user = new User({ email: "test@email.com" });
-    await expect(user.save()).to.be.rejectedWith(Error);
+    await expect(user.save()).to.be.rejectedWith("Please add a password");
   });
 
   it("cannot create with password < 6 chars", async () => {
     const user = new User({email: "test@email.com", password: "pass"})
-    await expect(user.save()).to.be.rejectedWith(Error);
+    await expect(user.save()).to.be.rejectedWith("Password needs to be at least 6 characters");
   })
 });
