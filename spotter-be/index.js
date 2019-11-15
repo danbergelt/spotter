b05adffab919dc6aea9bf23d3366bc8187c8271b
Dan <danbergelt@protonmail.com>
2019-11-14 23:42:16 -0500

    BE optimizations

--- spotter-be/index.js
+++ spotter-be/index.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const users = require("./routes/users");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+const colors = require("colors");
+const morgan = require("morgan");
+const { connectDB, connectTestDB } = require("./config/db");
 const errorHandler = require("./middleware/error");
 dotenv.config();
 
 // Connect to DB and run server
-connectDB();
+if (process.env.NODE_ENV === "development") {
+  connectTestDB();
+}
+
+if (process.env.NODE_ENV === "production") {
+  connectDB();
+}
+
 const app = express();
+
+// Dev logger
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Body parser
 app.use(express.json());
@@ -20,9 +34,12 @@ app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server started on port ${port} IN ${process.env.NODE_ENV} mode`)
+);
 
 // Unhandled rejection handling
 process.on("unhandledRejection", () => {
+  console.log(`Error: ${err.message}`.red)
   server.close(() => process.exit(1));
 });
