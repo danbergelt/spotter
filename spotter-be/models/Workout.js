b08b07fe3ac0b27fcda1baf64f3c5a733a0300ec
Dan <danbergelt@protonmail.com>
2019-11-15 17:20:48 -0500

    create workout test MODEL

--- spotter-be/models/Workout.js
+++ spotter-be/models/Workout.js
@@ -3,30 +3,33 @@ const Schema = mongoose.Schema;
 
 // Helper Schemas
 const TagSchema = new Schema({
-  color: String,
+  color: {
+    type: String,
+    required: [true, "Please add a tag color"]
+  },
   content: {
     type: String,
-    maxlength: [20, "Tag cannot be longer than 20 characters"]
+    maxlength: [20, "Tag content cannot be longer than 20 characters"]
   }
 });
 
 const ExerciseSchema = new Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, "Please add an exercise name"],
     maxlength: [40, "Exercise name cannot be longer than 40 characters"]
   },
   weight: {
     type: Number,
-    max: 2000
+    max: [2000, "2000 lb limit"]
   },
   sets: {
     type: Number,
-    max: 2000
+    max: [2000, "2000 lb limit"]
   },
   reps: {
     type: Number,
-    max: 2000
+    max: [2000, "2000 lb limit"]
   }
 });
 
--- spotter-be/test/MODEL_WorkoutCreate.test.js
+++ spotter-be/test/MODEL_WorkoutCreate.test.js
@@ -42,6 +42,119 @@ describe("Workout model creation", () => {
 
   it("cannot create with no title", async () => {
     const workout = new Workout({ ...template, title: undefined });
-    await expect(workout.save()).to.be.rejectedWith("Please add a workout title");
+    await expect(workout.save()).to.be.rejectedWith(
+      "Please add a workout title"
+    );
   });
+
+  it("cannot create long title", async () => {
+    const workout = new Workout({
+      ...template,
+      title:
+        "kfiopwjciowcjiowcjiowcjiojciowjfiowjciojwiofjweiofjeiowjfioecnionconco2cho"
+    });
+    await expect(workout.save()).to.be.rejectedWith(
+      "Title cannot be longer than 50 characters"
+    );
+  });
+
+  it("cannot create with long tag content", async () => {
+    const workout = new Workout({
+      ...template,
+      tags: [
+        {
+          color: "red",
+          content: "this content is too long for a tag! try again"
+        }
+      ]
+    });
+    await expect(workout.save()).to.be.rejectedWith(
+      "Tag content cannot be longer than 20 characters"
+    );
+  });
+
+  it("cannot create tag with no color", async () => {
+    const workout = new Workout({
+      ...template,
+      tags: [
+        {
+          color: undefined,
+          content: "content"
+        }
+      ]
+    });
+    await expect(workout.save()).to.be.rejectedWith("Please add a tag color");
+  });
+
+  it("cannot create exercise with no name", async () => {
+    const workout = new Workout({
+      ...template,
+      exercises: [
+        {
+          name: undefined
+        }
+      ]
+    });
+    await expect(workout.save()).to.be.rejectedWith(
+      "Please add an exercise name"
+    );
+  });
+
+  it("cannot create exercise with long name", async () => {
+    const workout = new Workout({
+      ...template,
+      exercises: [
+        {
+          name: "kiojkiojiojiohiughuygtyftydfrtdrtsrtdrtdftufyugiuhuh"
+        }
+      ]
+    });
+    await expect(workout.save()).to.be.rejectedWith(
+      "Exercise name cannot be longer than 40 characters"
+    );
+  });
+
+  it("cannot create exercise with large values", async () => {
+    // weight
+    let workout = new Workout({
+      ...template,
+      exercises: [
+        {
+          ...template.exercises[0],
+          weight: 2001
+        }
+      ]
+    });
+    await expect(workout.save()).to.be.rejectedWith(
+      "2000 lb limit"
+    );
+
+    // sets
+    workout = new Workout({
+      ...template,
+      exercises: [
+        {
+          ...template.exercises[0],
+          sets: 2001
+        }
+      ]
+    });
+    await expect(workout.save()).to.be.rejectedWith(
+      "2000 lb limit"
+    );
+
+    // reps
+    workout = new Workout({
+      ...template,
+      exercises: [
+        {
+          ...template.exercises[0],
+          reps: 2001
+        }
+      ]
+    });
+    await expect(workout.save()).to.be.rejectedWith(
+      "2000 lb limit"
+    );
+  })
 });
