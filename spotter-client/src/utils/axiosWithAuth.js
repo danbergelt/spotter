7dc63acbf941896e1184cc24694c93d16783a08b
Dan <danbergelt@protonmail.com>
2019-11-16 19:35:57 -0500

    workout range inserted in redux store, mapped to corresponding date

---
+++ spotter-client/src/utils/axiosWithAuth.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import secureStorage from './secureToken';
+
+export const axiosWithAuth = () => {
+  const token = secureStorage.getItem(`${process.env.REACT_APP_KEY}`);
+
+  return axios.create({
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
+}
