f241ab74e6db035004d348fdf47da20dcab1dfaf
Dan <danbergelt@protonmail.com>
2019-11-23 18:47:49 -0500

    tabbing functionality added to tag modal

--- spotter-client/src/components/dash/workoutmodal/tagsmodal/TagsModal.js
+++ spotter-client/src/components/dash/workoutmodal/tagsmodal/TagsModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "react-modal";
 
 import TagsModalHead from "./TagsModalHead";
@@ -6,21 +6,31 @@ import TagsModalHead from "./TagsModalHead";
 if (process.env.NODE_ENV !== "test") Modal.setAppElement("#root");
 
 const TagsModal = ({ modal, closeModal }) => {
-
   const customStyles = {
     overlay: {
       background: "transparent"
     },
     content: {
       width: "300px",
-      height: "200px",
-      marginLeft: '52.25vw'
+      height: "275px",
+      marginLeft: "60.5vw"
     }
   };
 
+  const [active, setActive] = useState(0);
+
   return (
-    <Modal style={customStyles} contentLabel="Tags Modal" isOpen={modal}>
-      <TagsModalHead />
+    <Modal
+      style={customStyles}
+      onRequestClose={closeModal}
+      contentLabel="Tags Modal"
+      isOpen={modal}
+    >
+      <TagsModalHead
+        closeModal={closeModal}
+        active={active}
+        setActive={setActive}
+      />
     </Modal>
   );
 };
--- spotter-client/src/components/dash/workoutmodal/tagsmodal/TagsModalHead.js
+++ spotter-client/src/components/dash/workoutmodal/tagsmodal/TagsModalHead.js
@@ -1,15 +1,36 @@
 import React from "react";
-import { FiX } from 'react-icons/fi';
+import { FiX } from "react-icons/fi";
 
-const TagsModalHead = () => {
+const TagsModalHead = ({ closeModal, active, setActive }) => {
   return (
     <div className="tags-modal-head-container">
       <div className="tags-modal-head-tabs">
-        <div>Add</div>
-        <div>Manage</div>
-        <div>Create</div>
+        <div
+          className={
+            active === 0 ? "tags-modal-tab-active" : "tags-modal-tab-not-active"
+          }
+          onClick={() => setActive(0)}
+        >
+          Add
+        </div>
+        <div
+          className={
+            active === 1 ? "tags-modal-tab-active" : "tags-modal-tab-not-active"
+          }
+          onClick={() => setActive(1)}
+        >
+          Manage
+        </div>
+        <div
+          className={
+            active === 2 ? "tags-modal-tab-active" : "tags-modal-tab-not-active"
+          }
+          onClick={() => setActive(2)}
+        >
+          Create
+        </div>
       </div>
-      <div className="tags-modal-head-exit">
+      <div onClick={closeModal} className="tags-modal-head-exit">
         <FiX />
       </div>
     </div>
