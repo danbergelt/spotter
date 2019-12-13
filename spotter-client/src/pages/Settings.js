67a2f4d541f1c2b2fe17b6de07d28c057bf21e9f
Dan <danbergelt@protonmail.com>
2019-12-12 23:03:32 -0500

    style updates, global margin resizing

--- spotter-client/src/pages/Settings.js
+++ spotter-client/src/pages/Settings.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import ChangePassword from "../components/settings/ChangePassword";
 
 const Settings = () => {
+  const [changePassword, setChangePassword] = useState(false);
+
   return (
     <div className="settings-container">
       <div className="settings-section">
         <div className="settings-head">Account Settings</div>
-        <div className="settings-action">Change password...</div>
+        <ChangePassword
+          changePassword={changePassword}
+          setChangePassword={setChangePassword}
+        >
+          <div
+            onClick={() => setChangePassword(true)}
+            className="settings-action"
+          >
+            Change password...
+          </div>
+        </ChangePassword>
       </div>
       <div className="settings-section">
         <div className="settings-head">Close Account</div>
         <div className="settings-del-confirm">
-          <span id="del-account-danger">Danger!</span> This action can't be undone. All of your data will be deleted, and you
-          will be redirected to the Signup screen.
+          <span id="del-account-danger">Danger!</span> This action can't be
+          undone. All of your data will be deleted, and you will be redirected
+          to the Signup screen.
           <br />
           <br />
           <br />
