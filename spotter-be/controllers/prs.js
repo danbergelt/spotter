11fa44e88f3008617d5bcacccf35c1ab79364324
Dan <danbergelt@protonmail.com>
2019-12-21 18:25:40 -0500

    added some comments for caching process

--- spotter-be/controllers/prs.js
+++ spotter-be/controllers/prs.js
@@ -10,15 +10,19 @@ const redis = require("redis"),
 // @route --> GET /api/auth/prs
 // @access --> Private
 
+// if the cache is stale, generate PRs
 exports.generatePrs = asyncHandler(async (req, res, next) => {
+  // find all workouts by this user, and select the date and exercises fields
   const workouts = await Workout.find({ user: req.user._id }).select(
     "exercises date"
   );
 
+  // find all saved exercises 
   const exercises = await Exercise.find({ user: req.user._id });
 
   let prs = {};
 
+  // for each exercise, map a key/value into the PRs object. This will be our returned data
   exercises.forEach(exercise => {
     prs[exercise.name] = { name: exercise.name };
   });
@@ -26,10 +30,16 @@ exports.generatePrs = asyncHandler(async (req, res, next) => {
   // O(NM)
   // need to explore replacements for this...although since I am traversing a matrix,
   // and need to iterate over each element, not sure what other options I have
+
+  // map over every workout
   workouts.forEach(workout =>
+    // map over every exercise in every workout
     workout.exercises.forEach(exercise => {
+      // instantiate an initial value for PR comparison
       let pr = -Infinity;
+      // if the exercise is saved, and the exercise weight is greater than the current PR value
       if (prs[exercise.name] && pr < exercise.weight) {
+        // save the weight as a PR, save the date, and save that we are caching the data
         prs[exercise.name].pr = exercise.weight;
         prs[exercise.name].date = workout.date;
         prs["stale"] = false;
@@ -37,6 +47,7 @@ exports.generatePrs = asyncHandler(async (req, res, next) => {
     })
   );
 
+  // cache the data
   await client.hset(
     req.user._id.toString(),
     "stale",
@@ -45,6 +56,7 @@ exports.generatePrs = asyncHandler(async (req, res, next) => {
     JSON.stringify(prs)
   );
 
+  // return the PRs to the client
   res.status(200).json({
     success: true,
     prs
--- spotter-be/middleware/cache.js
+++ spotter-be/middleware/cache.js
@@ -3,9 +3,11 @@ const redis = require("redis"),
   client = redis.createClient();
 
 exports.prCache = asyncHandler(async (req, res, next) => {
+  // before generating PRs manually, check to see if previous PR generation is stale
   await client.hgetall(req.user._id.toString(), async (err, cache) => {
     if (err) throw err;
 
+    // if the cache isn't stale, return the cached PRs
     if (cache.stale === "false") {
       res.status(200).json({
         success: true,
