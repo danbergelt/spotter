4f10a14c0cf7627a8f7754d3901c12da4c482643
Dan <danbergelt@protonmail.com>
2020-01-03 15:01:39 -0500

    homepage textblocks added

--- spotter-client/src/pages/Home.tsx
+++ spotter-client/src/pages/Home.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 import Hero from "../components/home/HomeHero";
+import TextBlocks from "../components/home/TextBlocks";
 
 const Home: React.FC = () => {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <TextBlocks />
+    </>
+  );
 };
 
 export default Home;
