791c81f96877f4f7cbddf8d99a871f398723a008
Dan <danbergelt@protonmail.com>
2020-01-02 23:57:03 -0500

    delete account tested on BE

--- spotter-be/src/test/utils/teardown.ts
+++ spotter-be/src/test/utils/teardown.ts
@@ -7,6 +7,8 @@ const flush = promisify(client.flushall).bind(client);
 after(async () => {
   await flush();
   //@ts-ignore
+  // these mongodb scripts are personalized to my local environment
+  // if cloned, replace this with your personal scripts to stop and start a mongodb docker container :)
   exec("stop mongodb && run-mongodb", (err, stdout, stderr) => {
     if (err) {
       return;
