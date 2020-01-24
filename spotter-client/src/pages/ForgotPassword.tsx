db2809f783772a375e31c880f91531b67c4c469d
Dan Bergelt <dan@danbergelt.com>
2020-01-24 12:46:58 -0500

    verified MG domain

--- spotter-client/src/pages/ForgotPassword.tsx
+++ spotter-client/src/pages/ForgotPassword.tsx
@@ -10,7 +10,7 @@ const ForgotPassword: React.FC = () => {
   return (
     <>
       <Helmet>
-        <title>Forgt Password | Spotter</title>
+        <title>Forgot Password | Spotter</title>
       </Helmet>
       {pathname === '/forgotpassword' ? (
         <ForgotPasswordInstructions />
