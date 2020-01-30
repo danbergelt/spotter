a71cfc0c678adcfbe3c5004ee34ba6202841a57f
Dan Bergelt <dan@danbergelt.com>
2020-01-30 01:25:58 -0500

    removed floating logs

--- spotter-client/src/App.tsx
+++ spotter-client/src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
         });
         setLoading(false);
       })
-      .catch(() => {
+      .catch(err => {
         history.push('/500');
         setLoading(false);
       });
